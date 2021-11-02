f7bde412f651088c6fccad00a3aa43a88249dfa6
PierreLvl <pierre.louvel@protonmail.com>
2021-11-02 20:14:18 +0100

    add uniswapRouter interface

---
+++ contracts/interfaces/IUniswapV2Router.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.8.0;
+
+interface IUniswapV2Router {
+    function addLiquidity(
+        address tokenA,
+        address tokenB,
+        uint amountADesired,
+        uint amountBDesired,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountA, uint amountB, uint liquidity);
+
+    function removeLiquidity(
+        address tokenA,
+        address tokenB,
+        uint liquidity,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountA, uint amountB);
+    
+    function swapExactTokensForTokens(
+        uint amountIn, 
+        uint amountOutMin, 
+        address[] calldata path, 
+        address to, 
+        uint deadline
+    ) external returns (uint[] memory amounts);
+}
