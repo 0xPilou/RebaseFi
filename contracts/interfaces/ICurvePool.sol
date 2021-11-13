1e25cec4277f05f579beeee2aca341816c916cf8
PierreLvl <pierre.louvel@protonmail.com>
2021-11-13 18:48:50 +0100

    fix CurvePool interface

--- contracts/interfaces/ICurvePool.sol
+++ contracts/interfaces/ICurvePool.sol
@@ -4,8 +4,8 @@ interface ICurvePool {
     function add_liquidity(uint256[3] calldata uamounts, uint256 min_mint_amount) external;
     function remove_liquidity(uint256 _amount, uint256[4] calldata min_uamounts) external;
     
-    function calc_token_amount(uint256[3] _amounts, bool is_deposit) external view returns (uint256);
+    function calc_token_amount(uint256[3] calldata _amounts, bool is_deposit) external view returns (uint256);
 
-    function underlying_coins() external view returns (address[3]);
+    function underlying_coins(uint256 _id) external view returns (address);
     function lp_token() external view returns (address);
 }
