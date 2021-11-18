f4100a1be0aad6f5ef565976b0148df2eea47d00
PierreLvl <pierre.louvel@protonmail.com>
2021-11-18 15:12:41 +0100

    add MooCurveZap interface

---
+++ contracts/interfaces/IMooCurveZap.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.0;
+
+interface IMooCurveZap {
+ 
+    function curve3Pool() external view returns (address);
+    function beefyVault() external view returns (address);
+    function curveLP() external view returns (address);
+    function underlyingTokens() external view returns (address[3]);
+
+    function zap(address _tokenToZap, uint256 _amountToZap) external;
+
+    function unzap(address _tokenToReceive, uint256 _amountToUnzap) external;
+
+    function calculateBestOption(uint256[3] calldata _amounts) external view returns (uint);
+}
