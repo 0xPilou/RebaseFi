38e417c46aebd38d7f362d6ab41ddac1be194211
PierreLvl <pierre.louvel@protonmail.com>
2021-12-08 13:49:16 +0100

    update Time Rebase Vault design - update Test coverage

--- contracts/TimeOptimizer.sol
+++ contracts/TimeOptimizer.sol
@@ -1,27 +1,26 @@
 pragma solidity ^0.8.0;
 
-import 'openzeppelin-solidity/contracts/utils/math/SafeMath.sol';
-import 'openzeppelin-solidity/contracts/utils/Context.sol';
-import 'openzeppelin-solidity/contracts/access/Ownable.sol';
-import 'openzeppelin-solidity/contracts/token/ERC20/utils/SafeERC20.sol';
+import "openzeppelin-solidity/contracts/utils/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/utils/Context.sol";
+import "openzeppelin-solidity/contracts/access/Ownable.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/utils/SafeERC20.sol";
 
-import './interfaces/ITimeStaking.sol';
-import './interfaces/IUniswapV2Router.sol';
-import './interfaces/IMooCurveZap.sol';
+import "./interfaces/ITimeStaking.sol";
+import "./interfaces/IUniswapV2Router.sol";
+import "./interfaces/IMooCurveZap.sol";
 
 contract TimeOptimizer is Ownable {
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
 
-    
     uint256 constant MAX_INT = 2**256 - 1;
 
     // Memo Under Management (MUM)
     uint256 public mum = 0;
-   
+
     /**
      * @dev Tokens addresses
-     */    
+     */
     address public MEMO = 0x136Acd46C134E8269052c62A67042D6bDeDde3C9;
     address public TIME = 0xb54f16fB19478766A268F172C9480f8da1a7c9C3;
     address public WETH = 0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7;
@@ -31,38 +30,45 @@ contract TimeOptimizer is Ownable {
      */
     address public timeStakingAddr = 0x4456B87Af11e87E329AB7d7C7A246ed1aC2168B9;
     address public uniV2RouterAddr = 0x60aE616a2155Ee3d9A68541Ba4544862310933d4;
-    address public mooCurveZapAddr;
     address public parentFactory;
 
     /**
      * @dev Initializes the strategy for the given protocol
      */
-    constructor(
-        address _mooCurveZapAddr
-    ) { 
-        mooCurveZapAddr = _mooCurveZapAddr;
+    constructor() {
         parentFactory = msg.sender;
-
         IERC20(MEMO).safeApprove(timeStakingAddr, 0);
         IERC20(MEMO).safeApprove(timeStakingAddr, MAX_INT);
         IERC20(TIME).safeApprove(uniV2RouterAddr, 0);
-        IERC20(TIME).safeApprove(uniV2RouterAddr, MAX_INT);        
+        IERC20(TIME).safeApprove(uniV2RouterAddr, MAX_INT);
     }
 
     function deposit(uint256 _amount) external onlyOwner {
-        require(IERC20(MEMO).balanceOf(address(msg.sender)) >= _amount, "Insufficient balance");
+        require(
+            IERC20(MEMO).balanceOf(address(msg.sender)) >= _amount,
+            "Insufficient balance"
+        );
         IERC20(MEMO).safeTransferFrom(msg.sender, address(this), _amount);
         mum = mum.add(_amount);
     }
 
     function withdraw(uint256 _amount) external onlyOwner {
-        require(IERC20(MEMO).balanceOf(address(this)) >= _amount, "Insufficient balance");
+        require(
+            IERC20(MEMO).balanceOf(address(this)) >= _amount,
+            "Insufficient balance"
+        );
         IERC20(MEMO).safeTransfer(msg.sender, _amount);
         mum = IERC20(MEMO).balanceOf(address(this));
     }
 
-    function reinvestInToken(address _desiredToken, uint256 _basisPoint) external onlyOwner {
-        require(_desiredToken != MEMO && _desiredToken != TIME, "Cannot reinvest into MEMO or TIME");
+    function reinvestInToken(address _desiredToken, uint256 _basisPoint)
+        external
+        onlyOwner
+    {
+        require(
+            _desiredToken != MEMO && _desiredToken != TIME,
+            "Cannot reinvest into MEMO or TIME"
+        );
         require(_basisPoint <= 10000, "Incorrect Basis Point parameter");
 
         uint256 profit = (IERC20(MEMO).balanceOf(address(this))).sub(mum);
@@ -71,26 +77,33 @@ contract TimeOptimizer is Ownable {
         ITimeStaking(timeStakingAddr).unstake(amountToSwap, true);
         mum = IERC20(MEMO).balanceOf(address(this));
         _swapToken(_desiredToken);
-        IERC20(_desiredToken).safeTransfer(msg.sender, IERC20(_desiredToken).balanceOf(address(this)));
+        IERC20(_desiredToken).safeTransfer(
+            msg.sender,
+            IERC20(_desiredToken).balanceOf(address(this))
+        );
     }
 
-    function reinvestInProduct(address _desiredToken, uint256 _basisPoint) external onlyOwner {
-        require(_desiredToken != MEMO, "Cannot reinvest into MEMO");
+    function reinvestInMooCurve(address _mooCurveZap, uint256 _basisPoint)
+        external
+        onlyOwner
+    {
         require(_basisPoint <= 10000, "Incorrect Basis Point parameter");
 
         uint256 profit = (IERC20(MEMO).balanceOf(address(this))).sub(mum);
-        uint256 amountToSwap = profit.mul(_basisPoint).div(10000);
+        uint256 amountToReinvest = profit.mul(_basisPoint).div(10000);
 
-        ITimeStaking(timeStakingAddr).unstake(amountToSwap, true);
+        ITimeStaking(timeStakingAddr).unstake(amountToReinvest, true);
         mum = IERC20(MEMO).balanceOf(address(this));
-        _swapToken(_desiredToken);
-        _reinvestInMooCurve(_desiredToken);
+        _reinvestInMooCurve(_mooCurveZap);
     }
-    
+
     function recoverERC20(address _ERC20) external onlyOwner {
-        if(IERC20(_ERC20).balanceOf(address(this)) > 0){
-            IERC20(_ERC20).safeTransfer(msg.sender, IERC20(_ERC20).balanceOf(address(this)));
-        }        
+        if (IERC20(_ERC20).balanceOf(address(this)) > 0) {
+            IERC20(_ERC20).safeTransfer(
+                msg.sender,
+                IERC20(_ERC20).balanceOf(address(this))
+            );
+        }
     }
 
     function setUniV2Router(address _uniV2Router) external onlyOwner {
@@ -112,7 +125,7 @@ contract TimeOptimizer is Ownable {
         address[] memory wethToDesiredToken = new address[](2);
         wethToDesiredToken[0] = WETH;
         wethToDesiredToken[1] = _desiredToken;
-        IERC20(WETH).safeApprove(uniV2RouterAddr, MAX_INT);        
+        IERC20(WETH).safeApprove(uniV2RouterAddr, MAX_INT);
         IUniswapV2Router(uniV2RouterAddr).swapExactTokensForTokens(
             IERC20(WETH).balanceOf(address(this)),
             0,
@@ -122,12 +135,24 @@ contract TimeOptimizer is Ownable {
         );
     }
 
-    function _reinvestInMooCurve(address _tokenToInvest) internal {
-        uint256 amountToInvest = IERC20(_tokenToInvest).balanceOf(address(this));
-        IERC20(_tokenToInvest).approve(mooCurveZapAddr, amountToInvest);
-        IMooCurveZap(mooCurveZapAddr).zap(_tokenToInvest, amountToInvest);
-        address mooToken = IMooCurveZap(mooCurveZapAddr).beefyVault();
-        IERC20(mooToken).safeTransfer(msg.sender, IERC20(mooToken).balanceOf(address(this)));
+    function _reinvestInMooCurve(address _mooCurveZap) internal {
+        address[] memory timeToWeth = new address[](2);
+        timeToWeth[0] = TIME;
+        timeToWeth[1] = WETH;
+        IUniswapV2Router(uniV2RouterAddr).swapExactTokensForTokens(
+            IERC20(TIME).balanceOf(address(this)),
+            0,
+            timeToWeth,
+            address(this),
+            block.timestamp.add(600)
+        );
+        uint256 amountToInvest = IERC20(WETH).balanceOf(address(this));
+        IERC20(WETH).approve(_mooCurveZap, amountToInvest);
+        IMooCurveZap(_mooCurveZap).zap(WETH, amountToInvest);
+        address mooToken = IMooCurveZap(_mooCurveZap).beefyVault();
+        IERC20(mooToken).safeTransfer(
+            msg.sender,
+            IERC20(mooToken).balanceOf(address(this))
+        );
     }
-    
-}    
+}
--- test/1_test_TimeOptimizer.js
+++ test/1_test_TimeOptimizer.js
@@ -7,63 +7,63 @@ const truffleAssert = require('truffle-assertions');
 
 // npx hardhat test test\1_test_TimeOptimizer.js --network localhost
 
-describe("TimeOptimizer Unit Tests", function () {  
-    this.timeout(40000);
-    
-    /* ABIs */
-    const MEMOabi = require("../external_abi/avalanche/MEMO.json");   
-    const WETHabi = require("../external_abi/avalanche/WAVAX.json");
-    const MOOabi = require("../external_abi/avalanche/MOO.json");
-    const DAIabi = require("../external_abi/avalanche/DAI.json");
-    const USDCabi = require("../external_abi/avalanche/USDC.json");
+describe("TimeOptimizer Unit Tests", function () {
+  this.timeout(40000);
+
+  /* ABIs */
+  const MEMOabi = require("../external_abi/avalanche/MEMO.json");
+  const WETHabi = require("../external_abi/avalanche/WAVAX.json");
+  const MOOabi = require("../external_abi/avalanche/MOO.json");
+  const DAIabi = require("../external_abi/avalanche/DAI.json");
+  const USDCabi = require("../external_abi/avalanche/USDC.json");
 
 
-    /* Addresses */
-    const MEMO = "0x136acd46c134e8269052c62a67042d6bdedde3c9"; 
-    const WETH = "0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7";
-    const DAI = "0xd586E7F844cEa2F87f50152665BCbc2C279D8d70";
-    const USDC = "0xA7D7079b0FEaD91F3e65f86E8915Cb59c1a4C664";
+  /* Addresses */
+  const MEMO = "0x136acd46c134e8269052c62a67042d6bdedde3c9";
+  const WETH = "0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7";
+  const DAI = "0xd586E7F844cEa2F87f50152665BCbc2C279D8d70";
+  const USDC = "0xA7D7079b0FEaD91F3e65f86E8915Cb59c1a4C664";
 
-    const JOEROUTER = "0x60aE616a2155Ee3d9A68541Ba4544862310933d4"; 
-    const TIMESTAKING = "0x4456B87Af11e87E329AB7d7C7A246ed1aC2168B9";
-    const CURVEPOOL = "0x7f90122BF0700F9E7e1F688fe926940E8839F353"; 
-    const BEEFYVAULT = "0x79A44dc13e5863Cf4AB36ab13e038A5F16861Abc";
+  const JOEROUTER = "0x60aE616a2155Ee3d9A68541Ba4544862310933d4";
+  const TIMESTAKING = "0x4456B87Af11e87E329AB7d7C7A246ed1aC2168B9";
+  const CURVEPOOL = "0x7f90122BF0700F9E7e1F688fe926940E8839F353";
+  const BEEFYVAULT = "0x79A44dc13e5863Cf4AB36ab13e038A5F16861Abc";
 
-    /* Provider */
-    const provider = new ethers.providers.JsonRpcProvider();   
+  /* Provider */
+  const provider = new ethers.providers.JsonRpcProvider();
 
-    // Instantiating the existing mainnet fork contracts
-    weth = new ethers.Contract(WETH, WETHabi, provider);
-    memo = new ethers.Contract(MEMO, MEMOabi, provider);
-    moo = new ethers.Contract(BEEFYVAULT, MOOabi, provider);
-    dai = new ethers.Contract(DAI, DAIabi, provider);
-    usdc = new ethers.Contract(USDC, USDCabi, provider);
+  // Instantiating the existing mainnet fork contracts
+  weth = new ethers.Contract(WETH, WETHabi, provider);
+  memo = new ethers.Contract(MEMO, MEMOabi, provider);
+  moo = new ethers.Contract(BEEFYVAULT, MOOabi, provider);
+  dai = new ethers.Contract(DAI, DAIabi, provider);
+  usdc = new ethers.Contract(USDC, USDCabi, provider);
 
 
-    let timeOptimizer;
-    let TimeOptimizer;  
-    let mooCurveZap;
-    let MooCurveZap;  
+  let timeOptimizer;
+  let TimeOptimizer;
+  let mooCurveZap;
+  let MooCurveZap;
 
-    before(async function () {
+  before(async function () {
 
     // Resetting the Avalanche Hardhat Mainnet Fork Network to block 6729600
     await network.provider.request({
-        method: "hardhat_reset",
-        params: [
-          {
-            forking: {
-              jsonRpcUrl: `https://api.avax.network/ext/bc/C/rpc`,
-              blockNumber:6844000
-            },
+      method: "hardhat_reset",
+      params: [
+        {
+          forking: {
+            jsonRpcUrl: `https://api.avax.network/ext/bc/C/rpc`,
+            blockNumber: 6844000
           },
-        ],
+        },
+      ],
     });
 
     await hre.network.provider.request({
-        method: "hardhat_impersonateAccount",
-        params: ["0x6e0a0DF2d76B97c610e5B96c32CE53b8Ab4c856C"],
-      });
+      method: "hardhat_impersonateAccount",
+      params: ["0x6e0a0DF2d76B97c610e5B96c32CE53b8Ab4c856C"],
+    });
     whaleMEMO = await ethers.getSigner("0x6e0a0DF2d76B97c610e5B96c32CE53b8Ab4c856C");
 
     await hre.network.provider.request({
@@ -71,132 +71,128 @@ describe("TimeOptimizer Unit Tests", function () {
       params: ["0x91F83E3d6C37908454687517E748555E7ad0fe53"],
     });
     whaleWETH = await ethers.getSigner("0x91F83E3d6C37908454687517E748555E7ad0fe53");
-   
+
     // Define the signers required for the tests
-    [user, mooDeployer, nonOwner, _] = await ethers.getSigners();   
+    [user, mooDeployer, nonOwner, _] = await ethers.getSigners();
 
     // Deploy MooCurveZap contract
     MooCurveZap = await ethers.getContractFactory("MooCurveZap");
     mooCurveZap = await MooCurveZap.connect(mooDeployer).deploy(
-        CURVEPOOL,
-        BEEFYVAULT
+      CURVEPOOL,
+      BEEFYVAULT
     );
 
     // Deploy TimeOptimizer contract
     TimeOptimizer = await ethers.getContractFactory("TimeOptimizer");
-    timeOptimizer = await TimeOptimizer.connect(user).deploy(
-        TIMESTAKING,
-        JOEROUTER, 
-        mooCurveZap.address
-    );
+    timeOptimizer = await TimeOptimizer.connect(user).deploy();
 
     const memoDecimals = await memo.decimals();
     const amount = 100;
     const weiAmount = ethers.utils.parseUnits(amount.toString(), memoDecimals);
 
     await memo.connect(whaleMEMO).transfer(user.address, weiAmount);
-    });
+  });
 
-    // Impersonate a MEMO Whale to transfer MEMO to the TimeOptimizer contract to simulate the rebase
-    async function rebase() {
-        const memoDecimals = await memo.decimals();
-        const amount = ethers.utils.parseUnits("10", memoDecimals);
-        await memo.connect(whaleMEMO).transfer(timeOptimizer.address, amount)
-    };
+  // Impersonate a MEMO Whale to transfer MEMO to the TimeOptimizer contract to simulate the rebase
+  async function rebase() {
+    const memoDecimals = await memo.decimals();
+    const amount = ethers.utils.parseUnits("10", memoDecimals);
+    await memo.connect(whaleMEMO).transfer(timeOptimizer.address, amount)
+  };
 
-    it("should deposit MEMO Token into the TimeOptimizer Contract", async () => {
-    
-      const amount = await memo.balanceOf(user.address);    
+  it("should deposit MEMO Token into the TimeOptimizer Contract", async () => {
 
-      await memo.connect(user).approve(timeOptimizer.address, amount);
-      await timeOptimizer.connect(user).deposit(amount);    
+    const amount = await memo.balanceOf(user.address);
 
-      const timeOptBalance = await memo.balanceOf(timeOptimizer.address);
-      const mum = await timeOptimizer.mum();    
+    await memo.connect(user).approve(timeOptimizer.address, amount);
+    await timeOptimizer.connect(user).deposit(amount);
 
-      expect(amount).to.equal(timeOptBalance);
-      expect(amount).to.equal(mum);
-      expect(timeOptBalance).to.equal(mum);
-        
-      await rebase();
-    });
+    const timeOptBalance = await memo.balanceOf(timeOptimizer.address);
+    const mum = await timeOptimizer.mum();
 
-    it("should reinvest 50% of the rebase in DAI", async () => {
-      const basisPoint = 5000;
+    expect(amount).to.equal(timeOptBalance);
+    expect(amount).to.equal(mum);
+    expect(timeOptBalance).to.equal(mum);
 
-      const mooBalBefore = await moo.balanceOf(user.address);
-      const timeOptiBalBefore = await memo.balanceOf(timeOptimizer.address);
-      const mumBefore = await timeOptimizer.mum();
+    await rebase();
+  });
 
-      await timeOptimizer.connect(user).reinvest(dai.address, basisPoint);
+  it("should reinvest 50% of the rebase in Curve Beefy Vault", async () => {
+    const basisPoint = 5000;
 
-      const mooBalAfter = await moo.balanceOf(user.address);
-      const timeOptiBalAfter = await memo.balanceOf(timeOptimizer.address);
+    const mooBalBefore = await moo.balanceOf(user.address);
+    const timeOptiBalBefore = await memo.balanceOf(timeOptimizer.address);
+    const mumBefore = await timeOptimizer.mum();
 
-      expect(mooBalBefore < mooBalAfter).to.equal(true)
-      expect(timeOptiBalAfter).to.equal(timeOptiBalBefore.sub(mumBefore).mul(basisPoint).div(10000).add(mumBefore))
+    await timeOptimizer.connect(user).reinvestInMooCurve(mooCurveZap.address, basisPoint);
 
-    });
+    const mooBalAfter = await moo.balanceOf(user.address);
+    const timeOptiBalAfter = await memo.balanceOf(timeOptimizer.address);
 
-    it("should recover the lost / airdropped WETH from the TimeOptimizer contract", async () => {
+    expect(mooBalBefore < mooBalAfter).to.equal(true)
+    expect(timeOptiBalAfter).to.equal(timeOptiBalBefore.sub(mumBefore).mul(basisPoint).div(10000).add(mumBefore))
 
-      const amountToTransfer = 10;
-      const weiAmountToTransfer = ethers.utils.parseEther(amountToTransfer.toString());
-      await weth.connect(whaleWETH).transfer(timeOptimizer.address, weiAmountToTransfer);
-  
-      // Checking the balances before the recovery operation
-      const optiWethBalBefore = await weth.balanceOf(timeOptimizer.address);
-      const userWethBalBefore = await weth.balanceOf(user.address);
-  
-      // ERC20 Recovery Operation
-      await timeOptimizer.connect(user).recoverERC20(weth.address);
-  
-      // Checking the balances after the recovery operation
-      const optiWethBalAfter = await weth.balanceOf(timeOptimizer.address);
-      const userWethBalAfter = await weth.balanceOf(user.address);
-  
-      // Assertion #1 : Optimizer Token C Balance Before > Optimizer Token C Balance After
-      expect(optiWethBalBefore > optiWethBalAfter).to.equal(true, "Optimizer Balance of WETH is incorrect");
-      expect(optiWethBalAfter).to.equal(0, "Optimizer Balance of WETH after recovery should be 0");
-        
-      // Assertion #2 : User Token C Balance Before < User Token C Balance After
-      expect(userWethBalAfter).to.equal(userWethBalBefore.add(weiAmountToTransfer), "User Balance of WETH is incorrect");
-    });
+  });
 
-    it("should prevent non-owner to interact with the contract", async () => { 
+  it("should recover the lost / airdropped WETH from the TimeOptimizer contract", async () => {
 
-      const amount = 10;
-      const weiAmount = ethers.utils.parseEther(amount.toString());
+    const amountToTransfer = 10;
+    const weiAmountToTransfer = ethers.utils.parseEther(amountToTransfer.toString());
+    await weth.connect(whaleWETH).transfer(timeOptimizer.address, weiAmountToTransfer);
 
-      // Assertion : Transaction should revert as the caller is not the owner of the contract
-      await truffleAssert.reverts(timeOptimizer.connect(nonOwner).deposit(weiAmount));
+    // Checking the balances before the recovery operation
+    const optiWethBalBefore = await weth.balanceOf(timeOptimizer.address);
+    const userWethBalBefore = await weth.balanceOf(user.address);
 
-      // Assertion : Transaction should revert as the caller is not the owner of the contract
-      await truffleAssert.reverts(timeOptimizer.connect(nonOwner).withdraw(weiAmount));
-      
-      // Assertion : Transaction should revert as the caller is not the owner of the contract
-      await truffleAssert.reverts(timeOptimizer.connect(nonOwner).reinvest(weth.address, 5000));
+    // ERC20 Recovery Operation
+    await timeOptimizer.connect(user).recoverERC20(weth.address);
 
-      // Assertion : Transaction should revert as the caller is not the owner of the contract
-      await truffleAssert.reverts(timeOptimizer.connect(nonOwner).recoverERC20(weth.address));
+    // Checking the balances after the recovery operation
+    const optiWethBalAfter = await weth.balanceOf(timeOptimizer.address);
+    const userWethBalAfter = await weth.balanceOf(user.address);
+
+    // Assertion #1 : Optimizer Token C Balance Before > Optimizer Token C Balance After
+    expect(optiWethBalBefore > optiWethBalAfter).to.equal(true, "Optimizer Balance of WETH is incorrect");
+    expect(optiWethBalAfter).to.equal(0, "Optimizer Balance of WETH after recovery should be 0");
+
+    // Assertion #2 : User Token C Balance Before < User Token C Balance After
+    expect(userWethBalAfter).to.equal(userWethBalBefore.add(weiAmountToTransfer), "User Balance of WETH is incorrect");
+  });
+
+  it("should prevent non-owner to interact with the contract", async () => {
+
+    const amount = 10;
+    const weiAmount = ethers.utils.parseEther(amount.toString());
+
+    // Assertion : Transaction should revert as the caller is not the owner of the contract
+    await truffleAssert.reverts(timeOptimizer.connect(nonOwner).deposit(weiAmount));
+
+    // Assertion : Transaction should revert as the caller is not the owner of the contract
+    await truffleAssert.reverts(timeOptimizer.connect(nonOwner).withdraw(weiAmount));
+
+    // Assertion : Transaction should revert as the caller is not the owner of the contract
+    await truffleAssert.reverts(timeOptimizer.connect(nonOwner).reinvestInMooCurve(mooCurveZap.address, 5000));
+
+    // Assertion : Transaction should revert as the caller is not the owner of the contract
+    await truffleAssert.reverts(timeOptimizer.connect(nonOwner).recoverERC20(weth.address));
   });
 
   it("should withdraw MEMO Token from the TimeOptimizer Contract", async () => {
-    
-    const amount = await memo.balanceOf(timeOptimizer.address);    
-    const userBalBefore = await memo.balanceOf(user.address);    
 
-    await timeOptimizer.connect(user).withdraw(amount);    
+    const amount = await memo.balanceOf(timeOptimizer.address);
+    const userBalBefore = await memo.balanceOf(user.address);
+
+    await timeOptimizer.connect(user).withdraw(amount);
 
     const timeOptBalance = await memo.balanceOf(timeOptimizer.address);
-    const userBalAfter = await memo.balanceOf(user.address);    
+    const userBalAfter = await memo.balanceOf(user.address);
 
-    const mum = await timeOptimizer.mum();    
+    const mum = await timeOptimizer.mum();
 
     expect(amount > timeOptBalance).to.equal(true);
     expect(userBalBefore < userBalAfter).to.equal(true);
     expect(mum).to.equal(0);
-      
+
     await rebase();
   });
 
